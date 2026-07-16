fb0d8fae61a8f5bf4b5c0cc05081eb0d9e130b3d
Nayeem Ahammed <nayeem0963@gmail.com>
2026-07-16 08:15:51 +0600

    my first react project

---
+++ README.md
@@ -0,0 +1,7 @@
+Version 4: Disable buttons when appropriate
+⏳ Version 5: Add a step selector (+1, +5, +10)
+⏳ Version 6: Save the counter after page refresh using localStorage
+⏳ Version 7: Split the app into reusable React components
+⏳ Version 8: Add attractive styling with CSS
+⏳ Version 9: Add animations
+⏳ Version 10: Build a professional-looking counter suitable for your portfolio
---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  
+const increase = () => {
+  setCount(prev => prev + step);
+};
+
+const decrease = () => {
+  setCount(prev => (prev >= step ? prev - step : 0));
+};
+
+
+const [step, setStep] = useState(1);
+
+    
+
+const reset = () => {
+  setCount(0);
+};
+
+
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+
+      <h2>{count}</h2>
+      
+      <button onClick={increase}>Increase by {step}</button>
+      <button onClick={decrease} disabled={count < step}>
+        Decrease by {step}
+      </button>
+      <button onClick={reset} disabled={count === 0}>
+        Reset
+      </button>
+
+      <label> Step: 
+<select
+  value={step}
+  onChange={(e) => setStep(Number(e.target.value))}
+>
+  <option value={1}>1</option>
+  <option value={5}>5</option>
+  <option value={10}>10</option>
+</select></label>
+    </div>
+  );
+}
+
+
+
+export default App; 
